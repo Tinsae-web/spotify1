e2340bfe30b7e7713c319d78b631b9bdc19e86c7
Tinsae Dejene <tens2009et@gmail.com>
2021-07-03 23:52:30 +0000

    My new commit

---
+++ Spotify.py
@@ -0,0 +1,25 @@
+import requests
+import spotipy
+CLIENT_ID = 'a5cbd7b7e02d417092d22de329849615'
+CLIENT_SECRET = '58e8d91d7efa4c0fbe43aef36520ccf2'
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+print(auth_response.status_code)
+auth_response_data = auth_response.json()
+auth_response_data
+access_token = auth_response_data['access_token']
+headers = {
+    'Authorization' : 'Bearer {token}'.format(token=access_token)
+}
+BASE_URL = 'https://api.spotify.com/v1/'
+track_id = '6CTWathupIiDs7U4InHnDA'
+r = requests.get(BASE_URL + 'audio-features/' + track_id, headers = headers)
+r = r.json()
+r
+
+
+
